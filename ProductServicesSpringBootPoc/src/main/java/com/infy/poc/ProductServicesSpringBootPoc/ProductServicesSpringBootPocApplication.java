f9ecb3f49905d21e1f672d1f2ee8ae526160bd24
satyagh2803 <47451521+satyagh2803@users.noreply.github.com>
2019-08-02 10:33:43 +0530

    added extends SpringBootServletInitializer

--- ProductServicesSpringBootPoc/src/main/java/com/infy/poc/ProductServicesSpringBootPoc/ProductServicesSpringBootPocApplication.java
+++ ProductServicesSpringBootPoc/src/main/java/com/infy/poc/ProductServicesSpringBootPoc/ProductServicesSpringBootPocApplication.java
@@ -2,13 +2,13 @@ package com.infy.poc.ProductServicesSpringBootPoc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 //@ComponentScan(basePackages ="com.infy.poc")
-public class ProductServicesSpringBootPocApplication {
+public class ProductServicesSpringBootPocApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServicesSpringBootPocApplication.class, args);
